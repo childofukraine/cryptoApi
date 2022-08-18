64d029254406989055ff494426abbf0b573d19ad
childofukraine <98478383+childofukraine@users.noreply.github.com>
2022-08-18 15:32:17 +0300

    Delete src directory

--- src/funcs.ts
+++
@@ -1,9 +0,0 @@
-export const getOriginPrice = (price: number, percentChange: number) => {
-  let result: number;
-  if (Math.sign(percentChange) === 1) {
-    result = price / ((100 + percentChange) / 100);
-  } else {
-    result = price / ((100 - percentChange) / 100);
-  }
-  return result.toFixed(8);
-};
